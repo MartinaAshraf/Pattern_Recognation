eaa81dd2df07f07d561516dab4b47a1913de9b20
Martina  Ashraf <118466399+MartinaAshraf@users.noreply.github.com>
2026-05-02 02:41:59 +0300

    Delete section1 directory

--- section1/recognation.py
+++
@@ -1,4 +0,0 @@
-import cv2
-img=cv2.imread("image.jpg")
-cv2.imshow('image',img)  #'image' هو اسم النافذة img هي الصورة اللي عايزين نعرضها
-cv2.waitKey(0) #لرقم 0 معناه "استنى للأبد لحد ما المستخدم يضغط زر"
